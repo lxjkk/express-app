f74be9971e37a62a1a64c26bb5dfc092bbee7a12
lxjkk <lxj1999902@163.com>
2022-06-12 14:17:03 +0800

    update

--- db.config.js
+++ db.config.js
@@ -2,6 +2,7 @@ const env = {
     'dev': {database : 'lforum',user: 'root', password: 'root'},
     'pro': {database : 'lforumbb', user: 'lforumbb', password: 'root'}
 }
+console.log(process.env.NODE_ENV, env);
 module.exports = {
     host     : 'localhost',
     multipleStatements: true,
